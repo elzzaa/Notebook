837ea3db0c1e2bf465f4b7b22693dcda5b9eb7fd
Oksana Sadova <oksana.sadova@ucu.edu.ua>
2021-03-03 13:00:50 +0200

    Notebook

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\003ok\PycharmProjects\semestr2\notebook\.env
+set VIRTUAL_ENV=C:\Users\003ok\PycharmProjects\semestr2\lab4\notebook\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
